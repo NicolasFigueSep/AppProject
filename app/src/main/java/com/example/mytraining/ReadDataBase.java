c3644afa6908d0cb34f809126db894909e687bce
NicolasFigueSep <97692749+NicolasFigueSep@users.noreply.github.com>
2023-04-14 13:02:19 +0200

    ReadDataBase y si no funciona conexion a BD llena de otra cosa

--- app/src/main/java/com/example/mytraining/ReadDataBase.java
+++ app/src/main/java/com/example/mytraining/ReadDataBase.java
@@ -37,13 +37,22 @@ public ArrayList<Ejercicio> getAllExercise(){
             id = c.getInt(1);            
             name = c.getString(2);
             description = c.getString(3);            
-            listaEjercicios.add(new Ejercicio(id,name,descriptio,"",0));
+            listaEjercicios.add(new Ejercicio(id,name,description,"",0));
         } while (c.moveToNext()); 
         c.close();
     } catch (Exception e) {
         Log.e("tle99", e.getMessage());
-
+        llenaListaEjercicios()
         
     }
     return listaEjercicios;
 }
+public void llenaListaEjercicios(){
+        listaEjercicios = new ArrayList<Ejercicio>();
+        listaEjercicios.add(new Ejercicio(1,"Elevaciones de hombros con mancuernas","Sostén una mancuerna en cada mano y levanta los hombros hacia las orejas, manteniendo los brazos rectos. Mantén la posición durante un segundo y luego baja los hombros lentamente.",
+                "https://youtu.be/xeJjTRIYOIM",0.060));
+        listaEjercicios.add(new Ejercicio(2,"Remo con barra","Mantén la espalda recta y levanta la barra hacia tu pecho, manteniendo los codos cerca del cuerpo. Baja la barra lentamente y repite el ejercicio.",
+                "https://www.youtube.com/watch?v=G8l_8chR5BE",0.100));
+        listaEjercicios.add(new Ejercicio(3,"Encogimientos de hombros con cable"," Agarra un cable con una polea baja en cada mano y tira de ellos hacia arriba hasta que tus hombros estén cerca de tus oídos. Mantén la posición durante un segundo y luego baja lentamente los cables.",
+                "https://youtu.be/v7QczJNuWIg",0.060));
+    }
